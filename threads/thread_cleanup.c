a35c66c4b2774151028d56895729d3ee5a28a518
liyibo1110 <liyibo1110@sina.com>
2018-07-06 23:55:24 +0800

    增加代码

--- threads/thread_cleanup.c
+++ threads/thread_cleanup.c
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <stdbool.h>
 
-static pthread_cond_t connd = PTHREAD_COND_INITIALIZER;
+static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
 static int glob = 0;
 
@@ -20,10 +20,49 @@ static void cleanupHandler(void *arg){
 
 static void *threadFunc(void *arg){
     
+    void *buf = malloc(0x10000);
+    printf("thread: allocated memory at %p\n", buf);
+
+    int s = pthread_mutex_lock(&mtx);
+    if(s != 0)  errExitEN(s, "pthread_mutex_lock");
+    pthread_cleanup_push(cleanupHandler, buf);
+
+    //等待全局变量glob不为0,才结束
+    while(glob == 0){
+        s = pthread_cond_wait(&cond, &mtx);
+        if(s != 0)  errExitEN(s, "pthread_cond_wait");
+    }
+    pthread_cleanup_pop(1); //不管线程有没有被取消，都会执行cleanup函数进行清理
     return NULL;
 }
 
 int main(int argc, char *argv[]){
+
+    pthread_t tid;
+    int s = pthread_create(&tid, NULL, threadFunc, NULL);
+    if(s != 0)  errExitEN(s, "pthread_create");
+
+    sleep(2);   //让子线程运行到wait阻塞
+
+    if(argc == 1){  //取消子线程
+        printf("main:   about to cancel thread\n");
+        s = pthread_cancel(tid);
+        if(s != 0)  errExitEN(s, "pthread_cancel");
+    }else{  //让子线程继续执行完毕
+        printf("main:   about to signal condition variable\n");
+        glob = 1;
+        s = pthread_cond_signal(&cond);
+        if(s != 0)  errExitEN(s, "pthread_cond_signal");
+    }
+
+    void *res;
+    s = pthread_join(tid, &res);
+    if(s != 0)  errExitEN(s, "pthread_join");
+    if(res == PTHREAD_CANCELED){
+        printf("main:   thread was canceled\n");
+    }else{
+        printf("main:   thread terminated normally\n");
+    }
 
     exit(EXIT_SUCCESS);
 }
--- timers/ptmr_sigev_signal.c
+++ timers/ptmr_sigev_signal.c
@@ -1,3 +1,4 @@
+#define _POSIX_C_SOURCE 199309
 #include "../lib/error_functions.h"
 #include "../time/curr_time.h"
 #include "./itimerspec_from_str.h"
@@ -7,8 +8,53 @@
 #include <string.h>
 #include <sys/types.h>
 #include <signal.h>
-#include <sys/time.h>
 #include <time.h>
 #include <stdbool.h>
 
-#define TIMER_SIG SIGRTMAX  //自定义信号
+#define TIMER_SIG SIGRTMAX  //自定义实时信号
+
+static void handler(int sig, siginfo_t *si, void *uc){
+    
+    timer_t *tidptr = si->si_value.sival_ptr;
+
+    printf("[%s] Got signal %d\n", currTime("%T"), sig);
+    printf("    *sival_ptr         = %ld\n", (long)*tidptr);
+    printf("    timer_getoverrun() = %d\n", timer_getoverrun(*tidptr));
+}
+
+int main(int argc, char *argv[]){
+    
+    if(argc < 2){
+        usageErr("%s secs[/nsecs][:int-secs[/int-nsecs]]...\n", argv[0]);
+    }
+
+    timer_t *tidlist = calloc(argc - 1, sizeof(timer_t));
+    if(tidlist == NULL) errExit("calloc");
+
+    //弄信号处理器
+    struct sigaction sa;
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = handler;
+    sigemptyset(&sa.sa_mask);
+    if(sigaction(TIMER_SIG, &sa, NULL) == -1)   errExit("sigaction");
+
+    //创建定时器
+    struct sigevent sev;
+    sev.sigev_notify = SIGEV_SIGNAL;
+    sev.sigev_signo = TIMER_SIG;
+
+    struct itimerspec ts;
+    for(int i = 0; i < argc - 1; i++){
+        itimerspecFromStr(argv[i + 1], &ts);
+        sev.sigev_value.sival_ptr = &tidlist[i];    //当作数组
+        //开始计时
+        if(timer_create(CLOCK_REALTIME, &sev, &tidlist[i]) == -1)   errExit("timer_create");
+        printf("Timer ID: %ld (%s)\n", (long)tidlist[i], argv[i + 1]);
+
+        if(timer_settime(tidlist[i], 0, &ts, NULL) == -1)   errExit("timer_settime");
+    }
+
+    while(true){
+        pause();
+    }
+}
